f2f633a4df8bb9ea09be96f0a4d409ced27d648f
Arnaud <arnaud@status.im>
2026-08-28 10:19:39 +0400

    Update logos package downloader

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-package-downloader.url = "github:logos-co/logos-package-downloader?ref=5e1a843b11bc54c9ac9d445c4aaf6c041076d1c5";
+    logos-package-downloader.url = "github:logos-co/logos-package-downloader";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
