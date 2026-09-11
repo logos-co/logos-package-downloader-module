49ecc30fee5fec89eec2bd182e42bd71552399b9
Arnaud <arnaud@status.im>
2026-09-11 17:46:36 +0400

    Update doctests and dependencies

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-package-downloader.url = "github:logos-co/logos-package-downloader?rev=4964ebf28dcc207310f0e3a609857d460def65d4";
+    logos-package-downloader.url = "github:logos-co/logos-package-downloader?rev=c7fb2edfed79949b00c02d8b42f82385ef37244d";
     storage_module.url = "github:logos-co/logos-storage-module?rev=30ade568b46d66e5aff28d917f079a84388f6a4f";
     modules_state.url = "github:logos-co/logos-modules-state-module";
   };
